909653d85c96a9c7a25b093bfa6a04906decd01b
vuongpsht <34977724+vuongpsht@users.noreply.github.com>
2021-04-10 21:46:22 +0700

    update shim

--- shims-browserify.js
+++ shims-browserify.js
@@ -5,7 +5,7 @@ module.exports = {
   "inherits": "~2.0.1",
   "console-browserify": "^1.1.0",
   "constants-browserify": "^1.0.0",
-  "crypto-browserify": "~3.2.6",
+  "crypto-browserify": "3.2.6",
   "dns.js": "^1.0.1",
   "domain-browser": "~1.1.0",
   "events": "~1.0.0",
